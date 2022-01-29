366149a9c794082413db9145a4f72ea5bdddd749
CEO-CGS <hassanw2400@gmail.com>
2022-01-28 17:56:52 -0800

    safds

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://CEO-CGS.github.io',
+  url: 'https://ceo-cgs.github.io/pythonDocs',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
