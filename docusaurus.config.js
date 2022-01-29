8c8266079111803e3864acc59060d58115ee7205
CEO-CGS <hassanw2400@gmail.com>
2022-01-28 17:49:57 -0800

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://CEO-CGS.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  deploymentBranch: 'master',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'CEO-CGS', // Usually your GitHub org/user name.
+  projectName: 'pythonDocs', // Usually your repo name.
+  trailingSlash: false,
 
   presets: [
     [
